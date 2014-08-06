cb6bb4b78bc93bb1761a439d60e94089facee757
ogencoglu <oguzhangencoglu90@gmail.com>
2014-08-07 00:30:54 +0300

    category added

--- bioinformatics_textbook_track/1C.py
+++ bioinformatics_textbook_track/1C.py
@@ -7,6 +7,7 @@ Solution of problem with the ID 1C - Pattern Matching Problem in Rosalind.
 '''
 
 '''
+Category: Bioinformatics Textbook Track
 Problem : 1C - Pattern Matching Problem
 
 Given: A DNA string s of length at most 1000 bp.
